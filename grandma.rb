ed877501d534ae175dd1fe6f5a04d69134ec7336
Mike Barberry <mikebarberry@protonmail.com>
2020-09-25 05:20:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(sentence)
   if sentence === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
   elsif sentence == sentence.upcase
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
